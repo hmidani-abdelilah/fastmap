bea86a78c5af82f5313fbad02017a9427d564b56
FluffySnowman <thefluffysnowmanyt@gmail.com>
2022-04-21 18:35:09 +0530

    it isnt working

--- main.sh
+++ main.sh
@@ -184,9 +184,10 @@ do
 		echo "2 TERMINAL MODE ENABLED" | lolcat
 		echo "================================================================" | lolcat
 		ifconfig > /dev/pts/0
+		bash test.sh > /dev/pts/1
 		read -p "Enter interface to scan for networks on>>> " wifiaudit_0x1 > /dev/pts/0
 		read -p "Enter BSSID and MAC of station to deauthenticate" wifiaudit_1x1 > /dev/pts/1
-		airodump-ng eth0
+
 
 
 
--- test.sh
+++ test.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-tmux
-tmux split-window -h
-tmux select-window -t 1
-tmux send "echo "hi"" ENTER; 
-sleep 2
-tmux select-window -t 0
-echo "hello"
-sleep 2
+ifconfig
