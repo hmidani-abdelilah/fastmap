2592451a4320b91d769dc8ad41c5d1d77102ea22
fluuffy <thefluffysnowmanyt@gmail.com>
2022-05-07 12:58:39 +0530

    indenting

--- main.sh
+++ main.sh
@@ -243,21 +243,33 @@ do
 	elif [[ $prompt_input == "traffic" ]] ; then
 
 		echo "================================================================" | lolcat
+		
 		echo "You must have vnstat install for this"
 		echo "INTERFACES:"
+
 		ip a
+
 		read -p "Interface to collect data on>>> " traffic_interface_0x1
 		read -p "Hour(h)/5min(5)/pi()>>> " traffice_0x1
+
 		if [[ $traffice_0x1 == "h" ]] ; then
+
 			vnstati -h -i $traffic_interface_0x1 -o vnstati_hourly.png
 			echo "Graph saved to vnstati_hourly.png in this folder"
+
 		elif [[ $traffice_0x1 == "5" ]] ; then 
+
 			vnstati -5 -i $traffic_interface_0x1 -o vnstati_5.png
 			echo "Graph saved to vnstati_5.png in this folder"
+
 		elif [[ $traffice_0x1 == "pi" ]] ;  then
+
 			vnstati -s -i $traffic_interface_0x1 -o vnstati_pi.png
 			echo "Graph saved to vnstati_pi.png in this folder"
+
 		fi
+
+		echo "================================================================" | lolcat
 
 	elif [[ $prompt_input == "help" ]] ; then
 
