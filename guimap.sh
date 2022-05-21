241b7c1c9b363381136b58feb82b5757b05c66dc
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-21 16:40:38 +0530

    added sizes'

--- guimap.sh
+++ guimap.sh
@@ -5,14 +5,15 @@ read -p "Press enter to start" null0x1
 
 while [ 1 -lt 2 ] ; do  
     listval1=`zenity --list \
-        --title ="Command to execute" \
+        --title="Command to execute" \
         --column="Command" --column="Description" \
         arpscan "Performs network scan" \
         port "Performs port scan on specified host" \
         lp "Lists all open ports cleanly" \
         synscan "Performs syn scan" \
         scanon "Scans operating system based on MAC address of specified targets" \
-        quit "exits the program"`
+        quit "exits the program" \
+        --width="700" --height="500"`
 
     echo $listval1 > list1guimap.psv
     cat list1guimap.psv
@@ -20,9 +21,10 @@ while [ 1 -lt 2 ] ; do
         netmaphost=`zenity --entry \
             --title="Host/subnet to scan" \
             --text="Enter host/subnet to scan" \
-            --entry-text "192.168.1.1"`
+            --entry-text "192.168.1.1"
+            --width="300" --height="200"`
         nmap -sn $netmaphost > temp.txt
-        notify-send "Scan Completed!"
+        notify-send "ARP SCAN COMPLETED :)"
         zenity --text-info \
             title="Output" \
             --filename="temp.txt"
