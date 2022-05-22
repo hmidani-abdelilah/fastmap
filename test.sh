b502de7a3eb559ab57a074268b926e519afffe40
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-22 19:11:06 +0530

    changed idk

--- test.sh
+++ test.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-zenity --forms --title="Question" --add-entry="Question" &
-
-sleep 1
-
-window="$(xdotool search --name 'Question')"
-
-xdotool windowactivate "$window"
-
-xdotool type 'some text'
+pwd
