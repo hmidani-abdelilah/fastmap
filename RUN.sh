90bf38bb91f993767eca9e632de90f6483d4c889
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-25 10:33:39 +0530

    added updatign

---
+++ RUN.sh
@@ -0,0 +1,5 @@
+echo "UPDATING FASTMAP................................................." | randtype -t 5,12000 | lolcat
+bash update.sh
+echo "UPDATE COMPLETE :)..............................................." | randtype -t 5,12000 | lolcat
+echo "RUNNING FASTMAP" | randtype -t 5,12000 | lolcat
+bash fastmap.sh
