2f82ba7e2c90610071c0a550186594674c9dd324
FluffySnowman <thefluffysnowmanyt@gmail.com>
2022-06-23 22:05:55 +0530

    added rustscan link

--- fastmap.sh
+++ fastmap.sh
@@ -73,6 +73,8 @@ do
 	
 	elif [[ $prompt_input == "rscan" ]] ; then
 
+		echo "Make sure to download rustscan from https://github.com/RustScan/RustScan/releases before continuing with this command"
+
 		echo "Starting rust scan" | randtype -t 5,12000 | lolcat
 		read -p "Enter host+/subnet to scan>>> " rscan_var_0x1
 
